1c0e5df8fcdb4295a4bbb4f23820d0c9140eacd7
Andrew Struck-Marcell <astruckm@gmail.com>
2019-06-12 08:42:24 -0700

    Initial Commit

---
+++ With Mutt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  With Mutt
+//
+//  Created by ASM on 6/12/19.
+//  Copyright © 2019 ASM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
