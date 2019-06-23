8cd5358d61cd4c790d6d85a07675764361a533ba
Andrew Struck-Marcell <astruckm@gmail.com>
2019-06-23 11:30:31 -0700

    Added files

---
+++ With Mutt/BusinessSelectionViewController.swift
@@ -0,0 +1,50 @@
+//
+//  BusinessTableViewController.swift
+//  With Mutt
+//
+//  Created by ASM on 6/17/19.
+//  Copyright © 2019 ASM. All rights reserved.
+//
+
+import UIKit
+
+//class BusinessSelectionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return BusinessType.allCases.count
+//    }
+//
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "businessType", for: indexPath) as! BusinessTypeCell
+//        let label = BusinessType.allCases[indexPath.row].label
+//        cell.businessTypeName.text = label
+//        if label == "ホテル" {
+//            cell.businessTypeIcon.image = UIImage(named: "bed")
+//        } else {
+//            cell.businessTypeIcon.image = UIImage()
+//        }
+//
+//        return cell
+//    }
+//
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        self.dismiss(animated: true) {
+//            //assign search target to be BusinessType.allCases[indexPath.row] (but have some data structure for that
+//        }
+//    }
+//}
+
+//extension BusinessSelectionViewController: UITableViewDataSource {
+//
+//}
